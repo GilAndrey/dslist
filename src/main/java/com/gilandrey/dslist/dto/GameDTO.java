d99a8c32beac4ea43a7b26b6748b6bd832ab558e
Gil Andrey <gil_andrey@hotmail.com>
2025-01-21 18:17:34 -0300

    Get game by id

--- src/main/java/com/gilandrey/dslist/dto/GameDTO.java
+++ src/main/java/com/gilandrey/dslist/dto/GameDTO.java
@@ -1,46 +1,25 @@
-package com.gilandrey.dslist.entities;
+package com.gilandrey.dslist.dto;
 
-import jakarta.persistence.*;
+import com.gilandrey.dslist.entities.Game;
+import org.springframework.beans.BeanUtils;
 
-import java.util.Objects;
+public class GameDTO {
 
-@Entity
-@Table(name = "tb_game")
-public class Game {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-
-    // Change column name in database
-    @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platforms;
     private Double score;
     private String imgUrl;
-
-    @Column(columnDefinition = "TEXT")
     private String shortDescription;
-
-    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
-    // Constructor with no arguments
-    public Game(){
+    public GameDTO(){
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
-        this.id = id;
-        this.title = title;
-        this.year = year;
-        this.genre = genre;
-        this.platforms = platforms;
-        this.score = score;
-        this.imgUrl = imgUrl;
-        this.shortDescription = shortDescription;
-        this.longDescription = longDescription;
+    public GameDTO(Game entity) {
+        BeanUtils.copyProperties(entity, this);
     }
 
     public Long getId() {
@@ -79,8 +58,8 @@ public class Game {
         return platforms;
     }
 
-    public void setPlatforms(String platform) {
-        this.platforms = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public Double getScore() {
@@ -113,18 +92,5 @@ public class Game {
 
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Game game = (Game) o;
-        return Objects.equals(id, game.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
     }
 }
